ec1d842b5c75a22eb3cedec34d652554c36544cc
KaushikGithub007 <69728220+KaushikGithub007@users.noreply.github.com>
2020-09-14 10:16:24 +0530

    Add new proj

---
+++ HelloWorldTest-master/HelloWorldTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello BUSHYYYY");
+            Console.WriteLine("h");
+            ////
+        }
+    }
+}
